663d80c8c57e5c6cbce42ffcb96db3fb26ef1668
muratrahmikurtul <muratrahmikurtul@gmail.com>
2023-03-31 17:33:29 +0200

    Fix

--- src/BackgroundVideo.jsx
+++ src/BackgroundVideo.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import videoBg from './assets/background.mp4';
-import { useState } from "react";
 
 const BackgroundVideo = () => {
-    const [muted, setMuted] = useState(true);
-    const handleToggleMute = () => setMuted(current => !current);
-
     return (
         <div>
             <video loop autoPlay muted id="bg-video">
-                <button onClick={handleToggleMute} className="control">Unmute</button>
-                <source src="https://www.youtube.com/watch?v=hqDd7lN38hw" type="video/mp4"/>
+                <source src={videoBg} type="video/mp4"/>
             </video>
         </div>
     )
